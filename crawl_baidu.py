3f835b99e020dd95f145c56b6d57395e5e013392
bruce <626731411@qq.com>
2017-07-29 15:49:39 +0800

    完成百度贴吧爬虫的DEBUG,特别要注意正则表达式！

--- crawl_baidu.py
+++ crawl_baidu.py
@@ -46,6 +46,7 @@ class BDTB:
             url = self.baseURL + self.seeLZ +"&pn=" + str(pageNum)
             request = urllib2.Request(url)
             response = urllib2.urlopen(request)
+            print 'hi1'
             return response.read().decode("utf-8")
         except urllib2.URLError,e:
             if hasattr(e,"reason"):
@@ -54,9 +55,11 @@ class BDTB:
     #获取帖子标题
     def getTitle(self):
         page = self.getPage(1)
+        print 'hi2'
         pattern = re.compile('<h3 class=core_title_txt.*?>(.*?)</h3>', re.S)
         result = re.search(pattern,page)
         if result:
+
             return result.group(1).strip()
         else:
             return None
@@ -64,20 +67,24 @@ class BDTB:
     #获取帖子的页数
     def getPageNum(self):
         page = self.getPage(1)
+        #这个页数到底是如何确定的？通过标签里的共几页确定的
         pattern = re.compile('<li class=l_reply_num.*?</span>.*?<span.*?>(.*?)</span>', re.S)
         result = re.search(pattern,page)
         if result:
+            print result
             return result.group(1).strip()
         else:
             return None
 
     #获取每一层楼的内容，传入页面内容
     def getContent(self,page):
-        pattern = re.compile('<div id=post_content_.*?>(.*?)</div>', re.S)
+        print 'hi3'
+        pattern = re.compile('<div id="post_content_.*?>(.*?)</div>', re.S)
         items = re.findall(pattern,page)
+        print 'hi4'
         for item in items:
-            print item[0]
-        print self.tool.replace(items[0])
+            print 'hi5'
+            print item
 
 baseURL = "http://tieba.baidu.com/p/3138733512"
 bdtb = BDTB(baseURL,1)
